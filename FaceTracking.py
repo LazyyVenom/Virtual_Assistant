c6963c84863d9455d6e07b31599e6df95e54e6d6
Anubhav <choubey.anubhav253@gmail.com>
2024-07-06 16:14:03 +0530

    Basic part done

--- FaceTracking.py
+++ FaceTracking.py
@@ -4,7 +4,6 @@ import mediapipe as mp
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
-# For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as face_detection:
     while cap.isOpened():
@@ -12,17 +11,14 @@ with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence
         if not success:
             continue
 
-        # To improve performance, mark the image as not writeable to pass by reference
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = face_detection.process(image)
 
-        # Draw the face detection annotations on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.detections:
             for detection in results.detections:
-                # Extract the bounding box
                 bboxC = detection.location_data.relative_bounding_box
                 ih, iw, _ = image.shape
                 xmin = int(bboxC.xmin * iw)
@@ -30,11 +26,9 @@ with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence
                 width = int(bboxC.width * iw)
                 height = int(bboxC.height * ih)
                 
-                # Calculate center and radius for the circle
                 center_x, center_y = int(xmin + width / 2), int(ymin + height / 2)
                 radius = int(min(width, height) / 2)
                 
-                # Draw a circle around the face
                 cv2.circle(image, (center_x, center_y - 20), radius, (0, 255, 0), 2)
                 
                 scale = radius / 90
